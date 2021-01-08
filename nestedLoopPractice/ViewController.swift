c8d3d3be4c590435e671741cb0db12b975ea0ee6
BettyPan <betty19931218@gmail.com>
2021-01-08 13:30:33 +0800

    Initial Commit

---
+++ nestedLoopPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nestedLoopPractice
+//
+//  Created by BettyPan on 2021/1/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
